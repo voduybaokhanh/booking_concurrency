dca4b78c8d78188e35f30b3d32141c12a17aa3fc
vdbkhanh <khanhvo908@gmail.com>
2025-12-15 16:56:03 +0700

    setup docker với migation

---
+++ prisma/migrations/20251215095232_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "SeatStatus" AS ENUM ('AVAILABLE', 'HOLD', 'BOOKED');
+
+-- CreateEnum
+CREATE TYPE "BookingStatus" AS ENUM ('CONFIRMED', 'CANCELLED');
+
+-- CreateTable
+CREATE TABLE "Seat" (
+    "id" TEXT NOT NULL,
+    "status" "SeatStatus" NOT NULL,
+    "version" INTEGER NOT NULL DEFAULT 0,
+    "holdExpiresAt" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Seat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" TEXT NOT NULL,
+    "seatId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "status" "BookingStatus" NOT NULL,
+    "idempotencyKey" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Booking_idempotencyKey_key" ON "Booking"("idempotencyKey");
