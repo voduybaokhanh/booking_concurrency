cc5f1961d5a43bf08dce4b70ab2ea82235c30fc4
vdbkhanh <khanhvo908@gmail.com>
2025-12-15 17:05:16 +0700

    setup seat, booking, common

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SeatModule } from './seat/seat.module';
+import { BookingModule } from './booking/booking.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [SeatModule, BookingModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
